a38721805a56dd831343cc0ec82130f7d3e0538a
hit300b1 <hit300b1@gmail.com>
2022-01-28 08:03:48 +0530

    hello java is deleted

--- aspire2022/src/aspire2022/Hello.java
+++
@@ -1,7 +0,0 @@
-package aspire2022;
-//congragulation-- you have done it...............
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Welcome to JAVA....");
-	}
-}
