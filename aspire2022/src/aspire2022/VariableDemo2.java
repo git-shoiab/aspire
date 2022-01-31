6b7930cf1ab982959afe9e6de3e04137a2da243d
hit300b1 <hit300b1@gmail.com>
2022-01-31 07:44:28 +0530

    valid identifiers

---
+++ aspire2022/src/aspire2022/VariableDemo2.java
@@ -0,0 +1,31 @@
+package aspire2022;
+/*
+ * OBJECTIVES
+ * 1. Valid identifiers
+ */
+public class VariableDemo2 {
+	public static void main(String[] args) {
+		int d;//d is a valid name
+		int size;//size is a valid name
+		
+		int _size;//valid name -name can start with underscore
+		
+		int $size;//valid name - name can start with $
+		
+		int si2ze;//valid name -name can have numbers in between
+		
+		int si_2_$ze;//valid name - name can have _, & and number any where
+		
+		//INVALID
+		
+		//int 2size;//Invalid - name cannot start with number
+		//int &size;//Invalid - name cannot start with any other special characters other than _ and $
+		//int si%ze;//Invalid - name cannot have any other special characters in between other than _ ad $
+		
+		//int class;//Invalid - name cannot have keywords as identifier
+		
+		//the same is applicable for all the data types
+		
+		
+	}
+}
