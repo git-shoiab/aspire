a167d34d37e9cbe23731b7544f070d62390b6a47
hit300b1 <hit300b1@gmail.com>
2022-03-02 07:56:04 +0530

    2-mar

---
+++ aspire2022/src/ch6/ArgsDemo.java
@@ -0,0 +1,8 @@
+package ch6;
+//https://fluvid.com/videos/detail/mv1dViwBadI7xeG3p#.Yhw3w01eLl8.link
+public class ArgsDemo {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[4]);
+	}
+}
